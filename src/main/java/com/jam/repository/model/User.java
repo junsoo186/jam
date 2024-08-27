f030e8b45f252377e09f1697da102922f605c87f
haejujung <167273792+haejujung@users.noreply.github.com>
2024-08-27 14:23:02 +0900

    유저DTO수정

--- src/main/java/com/jam/repository/model/User.java
+++ src/main/java/com/jam/repository/model/User.java
@@ -1,6 +1,7 @@
 package com.jam.repository.model;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,5 +19,13 @@ public class User {
 	private String name;
 	private Date birthDate;
 	private String gender;
+	private String address;
+	private String nickName;
+	private String phoneNumber;
+	private String email;
+	private String password;
+	private String adminCheck;
+	private Timestamp createdAt;
+
 
 }
