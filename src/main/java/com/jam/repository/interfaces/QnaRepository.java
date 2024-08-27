dab67330fce2aa23434c2733700582a014394836
junsoo186 <junsoo186@gmail.com>
2024-08-27 14:33:03 +0900

    create-Q&A xml , 인터페이스 생성

---
+++ src/main/java/com/jam/repository/interfaces/QnaRepository.java
@@ -0,0 +1,17 @@
+package com.jam.repository.interfaces;
+
+import org.apache.ibatis.annotations.Mapper;
+
+// Q&A 인터페이스 
+@Mapper //qna.xml 매칭
+public interface QnaRepository {
+
+}
+/**
+ * Model 클래스 만들기 
+ * 1. Q&A 전제 선택
+ * 2. 사용자가 ->  관리자에게 Q 남기기
+ * 3. 관리자가 ->  사용자에게 A 남기기
+ * 4. 
+ */
+
