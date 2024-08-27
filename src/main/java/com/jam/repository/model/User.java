e43c98f79f6fddaba041f9311bf8aa45b78e54ae
wjdgns5 <rhfovkeh5@naver.com>
2024-08-27 14:08:05 +0900

    User DTO 생성중

---
+++ src/main/java/com/jam/repository/model/User.java
@@ -0,0 +1,22 @@
+package com.jam.repository.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class User {
+	
+	private String name;
+	private Date birthDate;
+	private String gender;
+
+}
