e86b9ebe1cec520bbb44e65b92571d50bb19c388
wjdgns5 <80444098+wjdgns5@users.noreply.github.com>
2024-09-02 11:39:43 +0900

    카카오 회원가입 중복체크

--- src/main/java/com/jam/dto/UserDTO.java
+++ src/main/java/com/jam/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.jam.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @ToString
 public class UserDTO {
 
--- src/main/java/com/jam/service/UserService.java
+++ src/main/java/com/jam/service/UserService.java
@@ -34,7 +34,7 @@ public class UserService {
 
 	}
 
-	/**  
+	/**
 	 * 로그인 기능
 	 * 
 	 * @param dto
@@ -48,5 +48,51 @@ public class UserService {
 		return user;
 
 	}
+
+	/**
+	 * 카카오 휴대폰 번호가 +82 10-1234-5678 인데 --> 010-1234-5678로 변경
+	 */
+	public String convertPhoneNumber(String internationalNumber) {
+
+		// 국제 전화번호 형식에서 국가 코드(+82)와 공백을 제거
+		if (internationalNumber.startsWith("+82")) {
+			// 국가 코드를 제거하고 남은 숫자를 추출
+			String localNumber = internationalNumber.substring(3).trim();
+			// 남은 번호가 '10'으로 시작하면 앞에 '0'을 추가하여 국내 형식으로 변환
+			if (localNumber.startsWith("10")) {
+				return "0" + localNumber;
+			}
+		}
+		// 다른 경우 기본적으로 입력을 그대로 반환 (기타 형식 처리 가능)
+		return internationalNumber;
+	}
+	
+	/**
+	 * 네이버 (카카오?) 휴대폰 번호가 010-1234-5678 인데 --> 01012345678 로 변경
+	 * @param internationalNumber 
+	 */
+	public String removeHyphens(String phoneNumber) {
+		return phoneNumber.replace("-", "");
+	}
+
+	
+
+	/**
+	 * 회원가입 시 이메일 중복체크
+	 * @param email
+	 */
+	
+	public int checkEemail(String email) {
+		// TODO Auto-generated method stub
+		int result = 0;
+		result = userRepository.findByUserEmail(email);
+		if(result == 0) {
+			System.out.println("생성가능");
+		} else {
+			System.out.println("생성 불가능");
+		}
+		return result;
+	}
+	
 
 }
