e9e63ca2a1630a83c279c4b8d3445921a0196d94
Minhyeok <alsgur6811@gmail.com>
2024-09-06 15:41:28 +0900

    create-staff page

---
+++ src/main/java/com/jam/controller/StaffController.java
@@ -0,0 +1,49 @@
+package com.jam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/staff")
+@RequiredArgsConstructor
+public class StaffController {
+	
+	@GetMapping("")
+	public String handleStaffMain() {
+		return "staff/main";
+	}
+	
+	
+	@GetMapping("/dashboard")
+	public String handleDashboard() {
+		
+		return "staff/dashboard";
+	}
+	
+	@GetMapping("/report")
+	public String handleReport() {
+		return "staff/report";
+	}
+	
+	@GetMapping("/payment-management")
+	public String handlePayment() {
+		return "staff/payment-management";
+	}
+	
+	@GetMapping("/support")
+	public String handleSupport() {
+		return "staff/support";
+	}
+	
+	@GetMapping("/event")
+	public String handleEvent() {
+		return "staff/event";
+	}
+	
+	
+	
+	
+}
