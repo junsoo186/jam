776acecc9c7863bb1385804e13b4917eabe34589
haejujung <167273792+haejujung@users.noreply.github.com>
2024-09-14 17:43:20 +0900

    차트구현완료 관리자페이지이동중

--- src/main/java/com/jam/controller/GraphController.java
+++ src/main/java/com/jam/controller/GraphController.java
@@ -1,16 +1,42 @@
 package com.jam.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.jam.repository.interfaces.GraphRepository;
+import com.jam.service.EventService;
+import com.jam.vo.GraphVO;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/getGraphData")
 public class GraphController {
-	
+
 	@Autowired
-	private GraphRepository graphrepository;
-	
-	
+	GraphRepository graphRepository;
+
+	@GetMapping
+	@ResponseBody
+	public String getGraphData() {
+		// GraphRepository에서 데이터를 가져와서 List에 저장
+		List<GraphVO> graphList = graphRepository.find();
+		System.out.println("graphList 데이터: " + graphList);
+
+		// Gson 객체를 사용해 List<GraphVO>를 JSON으로 변환
+		Gson gson = new Gson();
+		String json = gson.toJson(graphList);
+		System.out.println("json 데이터 : " + json);
+
+		return json; // 변환된 JSON 문자열을 반환
+	}
 
 }
--- src/main/java/com/jam/repository/interfaces/GraphRepository.java
+++ src/main/java/com/jam/repository/interfaces/GraphRepository.java
@@ -3,12 +3,15 @@ package com.jam.repository.interfaces;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.jam.vo.GraphVO;
 
 @Mapper
+@Repository
 public interface GraphRepository {
 	
-	public List<GraphVO> find(GraphVO vo);
+	public List<GraphVO> find();
+
 
 }
