514d5c22edc1d235aefe1f18478e1fe1164983ea
junsoo <junsoo186@gmail.com>
2024-09-25 09:09:30 +0900

    update-config

--- src/main/java/com/jam/config/WebMvcConfig.java
+++ src/main/java/com/jam/config/WebMvcConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import java.util.List;
+import java.util.ArrayList;
 import com.jam.handler.AuthInterceptor;
 
 import lombok.RequiredArgsConstructor;
@@ -18,11 +20,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final AuthInterceptor authInterceptor;
 	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(authInterceptor);
-//	}
+	// @Override
+	// public void addInterceptors(InterceptorRegistry registry) {
+	// 	List<String> excludeList = new ArrayList<>();
+	// 	excludeList.add("/write/workDetail");
+	// 	excludeList.add("/user/sign-in");
+	// 	excludeList.add("/css/**");
+	// 	excludeList.add("/images/**");
+	// 	excludeList.add("/");	
+	// 	excludeList.add("/js/**");
+	// 	registry.addInterceptor(authInterceptor).addPathPatterns("/**").excludePathPatterns(excludeList);
+	// }
 	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/uploads/**").addResourceLocations("src/main/resources/static/");
